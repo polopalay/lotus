f2a73da5bb2d30f97d27fd9c5247e60b4ff39073
polopalay <60103759+polopalay@users.noreply.github.com>
2021-06-29 03:19:11 +0700

    Update editorTools.js

--- src/components/tool/editorTools.js
+++ src/components/tool/editorTools.js
@@ -4,19 +4,19 @@ import Quote from '@editorjs/quote';
 import Marker from '@editorjs/marker';
 import InlineCode from '@editorjs/inline-code';
 import ListTool from '@editorjs/list';
-import Delimiter from '@editorjs/delimiter'
+import Delimiter from '@editorjs/delimiter';
 
 export function editorTools(uploadByFile, uploadByUrl) {
     return {
         delimiter: Delimiter,
         paragraph: {class: Paragraph, inlineToolbar: true},
-        quote: Quote,
+        quote: {class: Quote, inlineToolbar: true},
         marker: Marker,
         inlineCode: InlineCode,
-        list: ListTool,
+        list: {class: ListTool, inlineToolbar: true},
         image: {
             class: ImageTool,
             config: {uploader: {uploadByFile: uploadByFile, uploadByUrl: uploadByUrl}}
-        }
+        },
     }
 }
